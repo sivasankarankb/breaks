876748fb7678be9d08d1a9c1479b2287ee4f24cb
Sivasankaran K B <kbsiva7@gmail.com>
2020-04-26 21:46:25 +0530

    Protected app entry point with 'if main' check.

--- desktop/app.py
+++ desktop/app.py
@@ -5,9 +5,10 @@
 import ui
 import logic
 
-app_ui = ui.App()
-work_timer_ui = ui.WorkTimer(master=app_ui.get_top_level_window())
-work_timer_logic = logic.WorkTimer(work_timer_ui)
-
-app_ui.start()
-work_timer_logic.cleanup()
+if name == '__main__':
+    app_ui = ui.App()
+    work_timer_ui = ui.WorkTimer(master=app_ui.get_top_level_window())
+    work_timer_logic = logic.WorkTimer(work_timer_ui)
+    
+    app_ui.start()
+    work_timer_logic.cleanup()
