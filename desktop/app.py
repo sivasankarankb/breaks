6bc275fedeee4d777fab12ee470f8b17f44b3265
Sivasankaran K B <kbsiva7@gmail.com>
2020-05-30 21:27:58 +0530

    Testing the time graphing class.

--- desktop/app.py
+++ desktop/app.py
@@ -4,11 +4,30 @@
 
 import ui
 import logic
+import persistance
 
 if __name__ == '__main__':
     app_ui = ui.App()
     work_timer_ui = ui.WorkTimer(master=app_ui.get_top_level_window())
     work_timer_logic = logic.WorkTimer(work_timer_ui)
+
+    graph_data = []
+    work_data = persistance.WorkData()
+    data = work_data.load()
+
+    if data != None:
+        data = data[0][1]
+        colors = ['#ffcc00', '#0033cc']
+        icolor = 0
+        for event in data:
+            graph_data.append((event[2], colors[icolor]))
+            icolor = (icolor + 1) % 2
+
+    time_graph = ui.TimeGraph(master=app_ui.get_top_level_window())
+
+    time_graph.set_graph_data(graph_data)
+
+    time_graph.draw_graph()
 
     app_ui.start()
     work_timer_logic.cleanup()
