06064518135055db2ed09f3a847b236b5375c859
Sivasankaran K B <kbsiva7@gmail.com>
2020-05-30 21:17:04 +0530

    Added a (work) time graph drawing class. The work timer can now be placed anywhere in the UI grid.

--- desktop/ui.py
+++ desktop/ui.py
@@ -32,9 +32,13 @@ class App:
     def __exit(self): self.__tk.destroy()
 
 class WorkTimer:
-    def __init__(self, master = None):
+    def __init__(self, master = None, row=None, column=None):
+        params = {}
+        if row != None: params['row'] = row
+        if column != None: params['column'] = column
+
         self.__frame = ttk.Frame(master)
-        self.__frame.grid(sticky = tk.N + tk.S + tk.E + tk.W)
+        self.__frame.grid(sticky = tk.N + tk.S + tk.E + tk.W, **params)
         self.__create_widgets()
 
     def __create_widgets(self):
@@ -61,3 +65,54 @@ class WorkTimer:
 
     def set_timer_button_listener(self, listener = None):
         self.__timer_button_listener = listener
+
+class TimeGraph():
+    def __init__(self, master=None, row=None, column=None):
+        params = {}
+        if row != None: params['row'] = row
+        if column != None: params['column'] = column
+
+        self.__frame = ttk.Frame(master)
+        self.__frame.grid(sticky=tk.E + tk.W, **params)
+
+        self.__create_widgets()
+
+    def __create_widgets(self):
+        self.__frame.columnconfigure(0, weight=1)
+
+        self.__canvas = tk.Canvas(self.__frame, height=32)
+        self.__canvas.grid(sticky=tk.E + tk.W)
+
+        self.__graph_data = []
+        self.draw_graph()
+
+    def set_graph_data(self, data, normalise=True):
+        total = 0
+        for item in data: total += item[0]
+
+        if normalise:
+            normalised = []
+            for item in data: normalised.append((item[0] / total, item[1]))
+
+        else: normalised = data
+
+        self.__graph_data = normalised
+
+    def draw_graph(self):
+        try:
+            for item in self.__graph_ids: self.__canvas.delete(item)
+        except: pass
+
+        self.__graph_ids = []
+
+        x1, y1 = 3, 3
+        y2 = int(self.__canvas['height']) - 3
+        width = int(self.__canvas['width']) - 5
+
+        for point in self.__graph_data:
+            x2 = x1 + (width * point[0])
+            fill = point[1]
+            item = self.__canvas.create_rectangle(x1, y1, x2, y2, fill=fill)
+
+            self.__graph_ids.append(item)
+            x1 = x2
