f7bed387f16db847d9a1ae6e9a5cf9a706dc5692
Sivasankaran K B <kbsiva7@gmail.com>
2020-06-07 19:09:28 +0530

    Changed grid placeable expand option values.
    Values 'horizontal', 'vertical' and 'both' are supported.

--- desktop/app.py
+++ desktop/app.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     app_ui = ui.App()
     master = app_ui.get_nesting_window()
 
-    doing_now = ui.DoingNow(master=master)
+    doing_now = ui.DoingNow(master=master, expand='both')
 
-    work_timer_ui = ui.WorkTimer(master=master)
+    work_timer_ui = ui.WorkTimer(master=master, expand='horizontal')
     work_timer_logic = logic.WorkTimer(work_timer_ui)
 
     #work_time_viewer = ui.WorkTimeViewer(master=master, expand=True)
--- desktop/ui.py
+++ desktop/ui.py
@@ -36,7 +36,7 @@ class App:
     def __exit(self): self.__tk.destroy()
 
 class GridPlaceable:
-    def __init__(self, master = None, row=None, column=None, expand=False):
+    def __init__(self, master = None, row=None, column=None, expand=None):
         params = {}
         if row != None: params['row'] = row
         if column != None: params['column'] = column
@@ -44,12 +44,16 @@ class GridPlaceable:
         frame = ttk.Frame(master)
         frame.grid(sticky=tk.NSEW, **params)
 
-        if master != None and expand:
+        if master != None and expand != None:
             info = frame.grid_info()
             row = int(info['row'])
             col = int(info['column'])
-            master.rowconfigure(row, weight=1)
-            master.columnconfigure(col, weight=1)
+
+            if expand == 'horizontal' or expand == 'both':
+                master.columnconfigure(col, weight=1)
+
+            if expand == 'vertical' or expand == 'both':
+                master.rowconfigure(row, weight=1)
 
         self.initialise(frame)
 
@@ -196,3 +200,5 @@ class DoingNow(GridPlaceable):
         self.__current_task.grid(sticky=tk.NSEW)
 
         frame.grid_configure(pady=(0,8))
+        frame.rowconfigure(1, weight=1)
+        frame.columnconfigure(0, weight=1)
