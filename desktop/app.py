263f3aa27829b8f4791768df11628b1a32fdeaac
Sivasankaran K B <kbsiva7@gmail.com>
2022-10-23 21:58:56 +0530

    Move UI layout to main window class

--- desktop/app.py
+++ desktop/app.py
@@ -6,100 +6,19 @@ import tkinter as tk
 from tkinter import ttk
 
 import main_window
-import toolbar
 import work_timer
-import doing_now
-import todo_list
 import app_monitor
-import about_box
 
 if __name__ == '__main__':
     main_window = main_window.MainWindow()
-    master = main_window.get_nesting_window()
-    master.rowconfigure(0, weight=1)
-    master.columnconfigure(1, weight=1)
-
-    toolbar_container = ttk.Frame(master)
-
-    toolbar_container.grid(
-        sticky=tk.NSEW, pady=(0,16), padx=(0,16), row=0, column=0
+    
+    work_timer_logic = work_timer.WorkTimer(
+        main_window.get_work_timer_ui()
     )
     
-    toolbar = toolbar.Toolbar(
-        master=toolbar_container,
-        button_labels=['Home', 'To do', 'App Monitor', 'About'],
-        button_icons=['home', 'notes', 'screen', 'info'],
+    app_monitor_inst = app_monitor.AppMonitor(
+        main_window.get_app_monitor_ui()
     )
-
-    home_container = ttk.Frame(master)
-    home_container.grid(sticky=tk.NSEW, row=0, column=1)
-    home_container.rowconfigure(0, weight=1)
-    home_container.columnconfigure(0, weight=1)
-    
-    doing_now_container = ttk.Frame(home_container)
-    doing_now_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=0)
-    doing_now = doing_now.DoingNow(master=doing_now_container)
-
-    work_timer_container = ttk.Frame(home_container)
-    work_timer_container.grid(sticky=tk.NSEW, pady=(0,16), row=1, column=0)
-    work_timer_ui = work_timer.WorkTimerUI(master=work_timer_container)
-    work_timer_logic = work_timer.WorkTimer(work_timer_ui)
-    
-    todo_container = ttk.Frame(master)
-    todo_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=1)
-    to_do_list = todo_list.ToDoList(master=todo_container)
-
-    app_monitor_container = ttk.Frame(master)
-    app_monitor_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=1)
-    app_monitor_ui = app_monitor.AppMonitorUI(master=app_monitor_container)
-    app_monitor_inst = app_monitor.AppMonitor(app_monitor_ui)
-
-    about_box_container = ttk.Frame(master)
-    about_box_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=1)
-    about_box = about_box.AboutBox(master=about_box_container)
-
-    def ui_state_home():
-        global home_container
-        global todo_container, app_monitor_container, about_box_container
-        
-        home_container.grid()
-        todo_container.grid_remove()
-        app_monitor_container.grid_remove()
-        about_box_container.grid_remove()
-
-    def ui_state_todo():
-        global home_container
-        global todo_container, app_monitor_container, about_box_container
-        
-        home_container.grid_remove()
-        todo_container.grid()
-        app_monitor_container.grid_remove()
-        about_box_container.grid_remove()
-
-    def ui_state_monitor():
-        global home_container
-        global todo_container, app_monitor_container, about_box_container
-        
-        home_container.grid_remove()
-        todo_container.grid_remove()
-        app_monitor_container.grid()
-        about_box_container.grid_remove()
-
-    def ui_state_about():
-        global home_container
-        global todo_container, app_monitor_container, about_box_container
-        
-        home_container.grid_remove()
-        todo_container.grid_remove()
-        app_monitor_container.grid_remove()
-        about_box_container.grid()
-
-    ui_state_home()
-
-    toolbar.set_listener('Home', ui_state_home)
-    toolbar.set_listener('To do', ui_state_todo)
-    toolbar.set_listener('App Monitor', ui_state_monitor)
-    toolbar.set_listener('About', ui_state_about)
 
     main_window.start()
     work_timer_logic.cleanup()
--- desktop/main_window.py
+++ desktop/main_window.py
@@ -2,6 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 
 import config
+import toolbar
+import work_timer
+import doing_now
+import todo_list
+import app_monitor
+import about_box
 
 class MainWindow:
     def __setup_window(self):
@@ -31,6 +37,86 @@ class MainWindow:
         self.__setup_window()
         self.__setup_style()
 
+        master = self.__outer_frame
+        master.rowconfigure(0, weight=1)
+        master.columnconfigure(1, weight=1)
+
+        toolbar_container = ttk.Frame(master)
+
+        toolbar_container.grid(
+            sticky=tk.NSEW, pady=(0,16), padx=(0,16), row=0, column=0
+        )
+        
+        toolbar_obj = toolbar.Toolbar(
+            master=toolbar_container,
+            button_labels=['Home', 'To do', 'App Monitor', 'About'],
+            button_icons=['home', 'notes', 'screen', 'info'],
+        )
+
+        home_container = ttk.Frame(master)
+        home_container.grid(sticky=tk.NSEW, row=0, column=1)
+        home_container.rowconfigure(0, weight=1)
+        home_container.columnconfigure(0, weight=1)
+        
+        doing_now_container = ttk.Frame(home_container)
+        doing_now_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=0)
+        doing_now_obj = doing_now.DoingNow(master=doing_now_container)
+
+        work_timer_container = ttk.Frame(home_container)
+        work_timer_container.grid(sticky=tk.NSEW, pady=(0,16), row=1, column=0)
+        work_timer_ui = work_timer.WorkTimerUI(master=work_timer_container)
+        
+        todo_container = ttk.Frame(master)
+        todo_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=1)
+        to_do_list = todo_list.ToDoList(master=todo_container)
+
+        app_monitor_container = ttk.Frame(master)
+        app_monitor_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=1)
+        app_monitor_ui = app_monitor.AppMonitorUI(master=app_monitor_container)
+
+        about_box_container = ttk.Frame(master)
+        about_box_container.grid(sticky=tk.NSEW, pady=(0,16), row=0, column=1)
+        about_box_obj = about_box.AboutBox(master=about_box_container)
+
+        self.__home_container = home_container
+        self.__todo_container = todo_container
+        self.__app_monitor_container = app_monitor_container
+        self.__about_box_container = about_box_container
+        self.__work_timer_ui = work_timer_ui
+        self.__app_monitor_ui = app_monitor_ui
+        self.__toolbar = toolbar_obj # Obj not saved -> Toolbar icons GC'd 
+
+        self.__toolbar.set_listener('Home', self.__ui_state_home)
+        self.__toolbar.set_listener('To do', self.__ui_state_todo)
+        self.__toolbar.set_listener('App Monitor', self.__ui_state_monitor)
+        self.__toolbar.set_listener('About', self.__ui_state_about)
+
+        self.__ui_state_home()
+
+    def __ui_state_home(self):
+        self.__home_container.grid()
+        self.__todo_container.grid_remove()
+        self.__app_monitor_container.grid_remove()
+        self.__about_box_container.grid_remove()
+
+    def __ui_state_todo(self):
+        self.__home_container.grid_remove()
+        self.__todo_container.grid()
+        self.__app_monitor_container.grid_remove()
+        self.__about_box_container.grid_remove()
+
+    def __ui_state_monitor(self):
+        self.__home_container.grid_remove()
+        self.__todo_container.grid_remove()
+        self.__app_monitor_container.grid()
+        self.__about_box_container.grid_remove()
+
+    def __ui_state_about(self):
+        self.__home_container.grid_remove()
+        self.__todo_container.grid_remove()
+        self.__app_monitor_container.grid_remove()
+        self.__about_box_container.grid()
+
     def start(self): self.__tk.mainloop()
 
     def get_nesting_window(self): return self.__outer_frame
@@ -42,3 +128,7 @@ class MainWindow:
     def show_main_window(self): self.__tk.deiconify()
 
     def __exit(self): self.__tk.destroy()
+
+    def get_work_timer_ui(self): return self.__work_timer_ui
+
+    def get_app_monitor_ui(self): return self.__app_monitor_ui
