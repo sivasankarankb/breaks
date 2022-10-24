e15a8ecbce7506787085e6327f8e48ec345b0140
Sivasankaran K B <kbsiva7@gmail.com>
2022-10-24 20:43:51 +0530

    Create data dir if not exist on app start

--- desktop/app.py
+++ desktop/app.py
@@ -2,11 +2,17 @@
 
 # app.py - What ties it all together.
 
+import pathlib
+import config
+
 import main_window
 import work_timer
 import app_monitor
 
 if __name__ == '__main__':
+    data_dir = pathlib.Path(config.data_dir)
+    data_dir.mkdir(parents=True, exist_ok=True)
+
     main_window_obj = main_window.MainWindow()
     
     work_timer_logic = work_timer.WorkTimer(
